46b9370a023f0d37d172d9dfcc07afab181c386a
murosichka1213 <mamrosjvoit@gmail.com>
2024-11-12 18:57:59 +0200

    create form

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const leftPanel = document.getElementById("leftPanel");
 const closePanel = document.getElementById("closePanel");
 
 burgerMenu.addEventListener("click", () => {
-	leftPanel.style.transform = "translateX(-20vw)";
+	leftPanel.style.transform = "translateX(-30vw)";
 });
 
 closePanel.addEventListener("click", () => {
